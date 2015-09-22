8b3184c416bea4ddc340e4b28e93c58cef55f0bd
svrLinux <sv_voronin@inbox.ru>
2015-09-22 08:33:15 -0700

    onLesson

--- main.c
+++ main.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
-#include <strung.h>
+#include <string.h>
 
 
 int main (int i, char **v ) {
 
 int pid = 0;
+FILE *fl;
+char str [10];
+char name[BUFSIZ];
 
-if (i>=2) {
-printf ("not correct argument\n"); 
+
+if (i>=3) {
+printf ("not correct argument \n"); 
 return -1;
 }
 
 pid = atoi (v[1]); 
+sprintf (name, "/proc/%d/status", pid );
+fl = fopen ( name );
+if (fl == NULL) {printf ("file not open\n"); return -1;}
 
+//fread ( str, sizof(char), 5, name );
 
+printf ("%s\n", str);
 
+fclose (fl);
 return 0;
 }
