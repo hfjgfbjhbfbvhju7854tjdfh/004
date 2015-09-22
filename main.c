52f08ca10aaf13cef11d8917b775608cb615fd8d
svrLinux <sv_voronin@inbox.ru>
2015-09-22 07:48:14 -0700

    onLesson

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
+#include <stdio.h>
+#include <strung.h>
+
+
+int main (int i, char **v ) {
+
+int pid = 0;
+
+if (i>=2) {
+printf ("not correct argument\n"); 
+return -1;
+}
+
+pid = atoi (v[1]); 
 
 
 
-int main () {
 return 0;
 }
